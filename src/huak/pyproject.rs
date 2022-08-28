5598c26d77715c90f42646973f5d3edc79fbf8c6
Chris Pryer <cnpryer@gmail.com>
2022-08-27 23:53:44 -0400

    Add string conversion tests to pyproject module

--- src/huak/pyproject.rs
+++ src/huak/pyproject.rs
@@ -10,9 +10,9 @@ struct Main {
 impl fmt::Display for Main {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         writeln!(f, "[tool.huak]")?;
-        writeln!(f, "name = {}", self.name)?;
-        writeln!(f, "version = {}", self.version)?;
-        writeln!(f, "description = {}", self.description)?;
+        writeln!(f, "name = \"{}\"", self.name)?;
+        writeln!(f, "version = \"{}\"", self.version)?;
+        writeln!(f, "description = \"{}\"", self.description)?;
         writeln!(f, "authors = {:#?}", self.authors)
     }
 }
@@ -24,7 +24,19 @@ enum DependencyKind {
     Dev,
 }
 
-type DependencyList = Vec<String>;
+#[derive(Clone, PartialEq, Eq, Debug)]
+struct Dependency {
+    name: String,
+    version: String,
+}
+
+impl fmt::Display for Dependency {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} = \"{}\"", self.name, self.version)
+    }
+}
+
+type DependencyList = Vec<Dependency>;
 
 struct Dependencies {
     kind: DependencyKind,
@@ -38,8 +50,13 @@ impl fmt::Display for Dependencies {
             DependencyKind::Dev => "dev-",
         };
 
-        writeln!(f, "[tool.huak.{}]", prefix)?;
-        writeln!(f, "{:#?}", self.list)
+        writeln!(f, "[tool.huak.{}dependencies]", prefix)?;
+
+        for dep in &self.list {
+            writeln!(f, "{}", dep)?;
+        }
+
+        Ok(())
     }
 }
 
@@ -52,7 +69,7 @@ impl fmt::Display for BuildSystem {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         writeln!(f, "[build-system]")?;
         writeln!(f, "requires = {:#?}", self.requires)?;
-        writeln!(f, "build-backend = {}", self.backend)
+        writeln!(f, "build-backend = \"{}\"", self.backend)
     }
 }
 
@@ -75,7 +92,6 @@ impl fmt::Display for Toml {
     }
 }
 
-// TODO: Test string conversions.
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -86,6 +102,13 @@ mod tests {
         let version = "0.0.0".to_string();
         let description = "".to_string();
         let authors = vec![];
+        let string = "\
+[tool.huak]
+name = \"Test\"
+version = \"0.0.0\"
+description = \"\"
+authors = []
+";
 
         let data = Main {
             name: name.clone(),
@@ -98,12 +121,16 @@ mod tests {
         assert_eq!(data.version, version);
         assert_eq!(data.description, description);
         assert_eq!(data.authors, authors);
+        assert_eq!(data.to_string(), string);
     }
 
     #[test]
     fn dependencies() {
         let kind = DependencyKind::Main;
         let list = vec![];
+        let string = "\
+[tool.huak.dependencies]
+";
 
         let data = Dependencies {
             kind: kind.clone(),
@@ -112,12 +139,16 @@ mod tests {
 
         assert_eq!(data.kind, kind);
         assert_eq!(data.list, list);
+        assert_eq!(data.to_string(), string)
     }
 
     #[test]
     fn dev_dependencies() {
         let kind = DependencyKind::Dev;
         let list = vec![];
+        let string = "\
+[tool.huak.dev-dependencies]
+";
 
         let data = Dependencies {
             kind: kind.clone(),
@@ -126,12 +157,18 @@ mod tests {
 
         assert_eq!(data.kind, kind);
         assert_eq!(data.list, list);
+        assert_eq!(data.to_string(), string);
     }
 
     #[test]
     fn build_system() {
         let requires = vec![];
         let backend = "".to_string();
+        let string = "\
+[build-system]
+requires = []
+build-backend = \"\"
+";
 
         let data = BuildSystem {
             requires: requires.clone(),
@@ -140,5 +177,6 @@ mod tests {
 
         assert_eq!(data.requires, requires);
         assert_eq!(data.backend, backend);
+        assert_eq!(data.to_string(), string);
     }
 }
