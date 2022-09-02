14f9f276cd568665f7887ad2700f521d26e50cf0
Chris Pryer <14341145+cnpryer@users.noreply.github.com>
2022-09-01 22:37:32 -0400

    Update get_venv_module_path to return PathBuf (#85)

--- src/bin/huak/commands/test.rs
+++ src/bin/huak/commands/test.rs
@@ -1,6 +1,6 @@
 use super::utils::{run_command, subcommand};
 use clap::Command;
-use huak::errors::CliResult;
+use huak::errors::{CliError, CliResult};
 use huak::utils::get_venv_module_path;
 use std::env;
 
@@ -14,9 +14,18 @@ pub fn run() -> CliResult {
     let cwd_buff = env::current_dir()?;
     let cwd = cwd_buff.as_path();
 
-    let pytest_path = get_venv_module_path("pytest")?;
+    let ptyest_path = get_venv_module_path("pytest")?;
+    let pytest_path = match ptyest_path.to_str() {
+        Some(p) => p,
+        None => {
+            return Err(CliError::new(
+                anyhow::format_err!("failed to construct path to pytest module"),
+                2,
+            ))
+        }
+    };
 
-    run_command(&pytest_path, &[], cwd)?;
+    run_command(pytest_path, &[], cwd)?;
 
     Ok(())
 }
