20323511d6ef21fb569c4e6f1ec57889f2a4a54a
Chris Pryer <cnpryer@gmail.com>
2022-09-08 18:23:47 -0400

    Refactor lint command

--- src/bin/huak/commands/lint.rs
+++ src/bin/huak/commands/lint.rs
@@ -1,7 +1,7 @@
-use super::utils::{run_command, subcommand};
+use super::utils::subcommand;
 use clap::Command;
-use huak::errors::{CliError, CliResult};
-use huak::utils::get_venv_module_path;
+use huak::ops;
+use huak::{errors::CliResult, project::Project};
 use std::env;
 
 pub fn arg() -> Command<'static> {
@@ -11,36 +11,10 @@ pub fn arg() -> Command<'static> {
 // TODO: Use pyproject.toml or .flake8 to override configuration.
 pub fn run() -> CliResult {
     // This command runs from the context of the cwd.
-    let cwd_buff = env::current_dir()?;
-    let cwd = cwd_buff.as_path();
+    let cwd = env::current_dir()?;
+    let project = Project::new(cwd);
 
-    let flake8_path = get_venv_module_path("flake8")?;
-    let flake8_path = match flake8_path.to_str() {
-        Some(p) => p,
-        None => {
-            return Err(CliError::new(
-                anyhow::format_err!("failed to construct path to flake8 module"),
-                2,
-            ))
-        }
-    };
-    let mypy_path = get_venv_module_path("mypy")?;
-    let mypy_path = match mypy_path.to_str() {
-        Some(p) => p,
-        None => {
-            return Err(CliError::new(
-                anyhow::format_err!("failed to construct path to mypy module"),
-                2,
-            ))
-        }
-    };
-
-    run_command(
-        flake8_path,
-        &["--ignore", "E203,W503", "--exclude", ".venv"],
-        cwd,
-    )?;
-    run_command(mypy_path, &["."], cwd)?;
+    ops::lint::lint_project(&project)?;
 
     Ok(())
 }
