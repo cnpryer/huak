b321b8c82aebe732f685a12ae99afd7b6282c1a4
Ethan <56288341+FoundedNahte@users.noreply.github.com>
2022-09-30 15:49:56 -0400

    Update update.rs

--- src/bin/huak/commands/update.rs
+++ src/bin/huak/commands/update.rs
@@ -2,7 +2,6 @@ use huak::errors::CliResult;
 
 /// Run the `update` command.
 pub fn run(dependency: String) -> CliResult<()> {
-    //let _ = args.get_one::<String>("dependency").unwrap();
 
     unimplemented!()
 }
