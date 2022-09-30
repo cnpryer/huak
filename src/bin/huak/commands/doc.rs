85e469385e3e723316598949c2c94a26f2cdea46
Ethan <56288341+FoundedNahte@users.noreply.github.com>
2022-09-30 15:48:42 -0400

    Update doc.rs

--- src/bin/huak/commands/doc.rs
+++ src/bin/huak/commands/doc.rs
@@ -3,7 +3,6 @@ use huak::errors::CliResult;
 /// Run the `doc` command.
 pub fn run(is_check: bool) -> CliResult<()> {
     // TODO: Use is_check.
-    // let _ = args.get_one::<bool>("check").unwrap();
 
     unimplemented!()
 }
