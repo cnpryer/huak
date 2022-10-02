f0aaa002be9108bdc7d436d5f0b2947b92f98ca3
Chris Pryer <14341145+cnpryer@users.noreply.github.com>
2022-10-01 23:22:33 -0400

    Add install --all (#265)

--- src/bin/huak/commands/install.rs
+++ src/bin/huak/commands/install.rs
@@ -6,14 +6,14 @@ use huak::ops;
 use huak::project::Project;
 
 /// Run the `install` command.
-pub fn run() -> CliResult<()> {
+pub fn run(all: bool) -> CliResult<()> {
     let cwd = env::current_dir()?;
     let project = match Project::from(cwd) {
         Ok(p) => p,
         Err(e) => return Err(CliError::new(e, ExitCode::FAILURE)),
     };
 
-    if let Err(e) = ops::install::install_project_dependencies(&project, &[]) {
+    if let Err(e) = ops::install::install_project_dependencies(&project, all) {
         return Err(CliError::new(e, ExitCode::FAILURE));
     };
 
