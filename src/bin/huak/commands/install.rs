acc58b8770a9db591e6583823b007407741c5b1b
Chris Pryer <cnpryer@gmail.com>
2023-01-23 22:33:40 -0500

    Create venv from install command if none exists

--- src/bin/huak/commands/install.rs
+++ src/bin/huak/commands/install.rs
@@ -3,6 +3,7 @@ use std::process::ExitCode;
 
 use crate::errors::{CliError, CliResult};
 use huak::env::venv::Venv;
+use huak::errors::HuakError;
 use huak::ops;
 use huak::project::Project;
 
@@ -14,9 +15,17 @@ pub fn run(groups: Option<Vec<String>>, all: bool) -> CliResult<()> {
         Err(e) => return Err(CliError::new(e, ExitCode::FAILURE)),
     };
 
-    // TODO: Create .venv anyway?
-    let venv = Venv::from_path(project.root())
-        .map_err(|e| CliError::new(e, ExitCode::FAILURE))?;
+    // Attempt to locate the project's venv. If none is found, attempt to create one.
+    let venv = match Venv::from_path(project.root()) {
+        Ok(it) => it,
+        Err(HuakError::VenvNotFound) => {
+            let it = Venv::new(project.root().join(".venv"));
+            it.create()
+                .map_err(|e| CliError::new(e, ExitCode::FAILURE))?;
+            it
+        }
+        Err(e) => return Err(CliError::new(e, ExitCode::FAILURE)),
+    };
 
     ops::install::install_project_dependencies(
         &venv,
