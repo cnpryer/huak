89f21277a6503ddc285ae89177026af4bbc22f87
Chris Pryer <cnpryer@gmail.com>
2023-03-19 18:16:51 -0400

    Git init

--- src/huak/git.rs
+++ src/huak/git.rs
@@ -1,17 +1,23 @@
-use crate::error::HuakResult;
+use git2::Repository;
+
+use crate::{error::HuakResult, Error};
 use std::path::Path;
 
 /// Initialize a directory on a local system as a git repository.
 pub fn init(dir_path: impl AsRef<Path>) -> HuakResult<()> {
-    todo!()
+    Repository::init(dir_path).map_err(|e| Error::GitError(e))?;
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tempfile::tempdir;
 
     #[test]
     fn test_init() {
-        todo!()
+        let dir = tempdir().unwrap().into_path();
+        init(&dir).unwrap();
+        assert!(dir.join(".git").is_dir());
     }
 }
