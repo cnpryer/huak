c9048a3be74f4e26c5182b857e918f1b98cf0922
Chris Pryer <14341145+cnpryer@users.noreply.github.com>
2023-04-08 21:37:10 -0400

    Fix lib.rs module doc comments (#579)

--- src/huak/lib.rs
+++ src/huak/lib.rs
@@ -13,38 +13,45 @@
 ///! 3. Running tests
 ///! 4. Distributing the project
 ///!
-///!❯ huak help
+///!```zsh
+///! ❯ huak help
 ///!
-///!A Python package manager written in Rust inspired by Cargo.
+///! A Python package manager written in Rust inspired by Cargo.
 ///!
-///!Usage: huak [OPTIONS] <COMMAND>
+///! Usage: huak [OPTIONS] <COMMAND>
 ///!
-///!Commands:
-///!  activate    Activate the virtual envionrment
-///!  add         Add dependencies to the project
-///!  build       Build tarball and wheel for the project
-///!  completion  Generates a shell completion script for supported shells
-///!  clean       Remove tarball and wheel from the built project
-///!  fix         Auto-fix fixable lint conflicts
-///!  fmt         Format the project's Python code
-///!  init        Initialize the existing project
-///!  install     Install the dependencies of an existing project
-///!  lint        Lint the project's Python code
-///!  new         Create a new project at <path>
-///!  lish     Builds and uploads current project to a registry
-///!  python      Manage Python installations
-///!  remove      Remove dependencies from the project
-///!  run         Run a command within the project's environment context
-///!  test        Test the project's Python code
-///!  update      Update the project's dependencies
-///!  version     Display the version of the project
-///!  help        Print this message or the help of the given subcommand(s)
+///! Commands:
+///!   activate    Activate the virtual envionrment
+///!   add         Add dependencies to the project
+///!   build       Build tarball and wheel for the project
+///!   completion  Generates a shell completion script for supported shells
+///!   clean       Remove tarball and wheel from the built project
+///!   fix         Auto-fix fixable lint conflicts
+///!   fmt         Format the project's Python code
+///!   init        Initialize the existing project
+///!   install     Install the dependencies of an existing project
+///!   lint        Lint the project's Python code
+///!   new         Create a new project at <path>
+///!   lish        Builds and uploads current project to a registry
+///!   python      Manage Python installations
+///!   remove      Remove dependencies from the project
+///!   run         Run a command within the project's environment context
+///!   test        Test the project's Python code
+///!   update      Update the project's dependencies
+///!   version     Display the version of the project
+///!   help        Print this message or the help of the given subcommand(s)
 ///!
-///! Options:
-///!   -q, --quiet    
-///!   -h, --help     Print help
-///!   -V, --version  Print version
-///
+///!  Options:
+///!    -q, --quiet    
+///!    -h, --help     Print help
+///!    -V, --version  Print version
+///!```
+mod error;
+mod fs;
+mod git;
+pub mod ops;
+mod sys;
+
 pub use error::{Error, HuakResult};
 use indexmap::IndexMap;
 use pep440_rs::{Operator, Version as PEP440Version, VersionSpecifiers};
@@ -67,11 +74,6 @@ use std::{
 use sys::Terminal;
 pub use sys::{TerminalOptions, Verbosity};
 use toml::Table;
-mod error;
-mod fs;
-mod git;
-pub mod ops;
-mod sys;
 
 const DEFAULT_VENV_NAME: &str = ".venv";
 const VENV_CONFIG_FILE_NAME: &str = "pyvenv.cfg";
@@ -167,8 +169,8 @@ impl Workspace {
         Environment::new()
     }
 
-    /// Get the current `Package`. The current `Package` is one found by its
-    /// metadata file nearest based on the `Workspace`'s `Config` data.
+    /// Get the current `Package`. The current `Package` is one found by its metadata file nearest based
+    /// on the `Workspace`'s `Config` data.
     fn current_package(&self) -> HuakResult<Package> {
         // Currently only pyproject.toml `LocalMetadata` file is supported.
         let metadata = self.current_local_metadata()?;
