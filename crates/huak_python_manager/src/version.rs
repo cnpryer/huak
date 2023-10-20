e44697388337c8b34c63dd8065f409219a4a6e20
Chris Pryer <cnpryer@gmail.com>
2023-10-20 17:19:56 -0400

    Refactor `is_requested_version` as `RequestedVersion.matches_version`

--- crates/huak_python_manager/src/version.rs
+++ crates/huak_python_manager/src/version.rs
@@ -1,6 +1,8 @@
 use anyhow::Error; // TODO(cnpryer): Library code should use thiserror
 use std::str::FromStr;
 
+use crate::releases::Version;
+
 #[derive(Debug, Clone)]
 pub(crate) struct RequestedVersion {
     pub(crate) major: Option<u8>,
@@ -21,5 +23,14 @@ impl FromStr for RequestedVersion {
             minor: parts.next(),
             patch: parts.next(),
         })
+    }
+}
+
+impl RequestedVersion {
+    /// Evaluates if some Python release's version is what was requested.
+    pub(crate) fn matches_version(&self, version: Version) -> bool {
+        self.major.map_or(true, |it| it == version.major)
+            && self.minor.map_or(true, |it| it == version.minor)
+            && self.patch.map_or(true, |it| it == version.patch)
     }
 }
